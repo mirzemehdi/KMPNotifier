3efb5214effa8ded26c681e22fa9596d442ae57d
Mirzamehdi <mirzemehdi@gmail.com>
2023-11-12 05:58:26 +0100

    Adding workflow action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -9,6 +12,82 @@ plugins {
     alias(libs.plugins.kotlinx.binary.validator)
 }
 
-allprojects{
+group = "io.github.mirzemehdi"
+version = "0.1.0-SNAPSHOT"
+
+allprojects {
+    val sonatypeUsername = gradleLocalProperties(rootDir).getProperty("sonatypeUsername")
+    val sonatypePassword = gradleLocalProperties(rootDir).getProperty("sonatypePassword")
+    val gpgKeySecret = gradleLocalProperties(rootDir).getProperty("gpgKeySecret")
+    val gpgKeyPassword = gradleLocalProperties(rootDir).getProperty("gpgKeyPassword")
+
+    val excludedModules = listOf(":sample")
+    if (project.path in excludedModules) return@allprojects
+
     apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven {
+                val isSnapshot = version.toString().endsWith("SNAPSHOT")
+                url = uri(
+                    if (!isSnapshot) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
+                    else "https://s01.oss.sonatype.org/content/repositories/snapshots"
+                )
+
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
+            }
+        }
+
+        val javadocJar = tasks.register<Jar>("javadocJar") {
+            dependsOn(tasks.getByName<DokkaTask>("dokkaHtml"))
+            archiveClassifier.set("javadoc")
+            from("${layout.buildDirectory}/dokka")
+        }
+
+        publications {
+            withType<MavenPublication> {
+                artifact(javadocJar)
+                pom {
+                    name.set("KMPNotifier")
+                    description.set(" Kotlin Multiplatform Notification Library targeting ios and android")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://opensource.org/licenses/Apache-2.0")
+                        }
+                    }
+                    url.set("mirzemehdi.github.io/KMPNotifier/")
+                    issueManagement {
+                        system.set("Github")
+                        url.set("https://github.com/mirzemehdi/KMPNotifier/issues")
+                    }
+                    scm {
+                        connection.set("https://github.com/mirzemehdi/KMPNotifier.git")
+                        url.set("https://github.com/mirzemehdi/KMPNotifier")
+                    }
+                    developers {
+                        developer {
+                            name.set("Mirzamehdi Karimov")
+                            email.set("mirzemehdi@gmail.com")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    val publishing = extensions.getByType<PublishingExtension>()
+    extensions.configure<SigningExtension> {
+        useInMemoryPgpKeys(gpgKeySecret, gpgKeyPassword)
+        sign(publishing.publications)
+    }
 }
+
+
