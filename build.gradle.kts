46f9aaed1365003d72955aad03cc3fead4d3696a
Mirzamehdi <mirzemehdi@gmail.com>
2024-02-17 10:06:17 +0100

    Updating api

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 allprojects {
     group = "io.github.mirzemehdi"
-    version = "0.2.0"
+    version = "0.3.0"
     val sonatypeUsername = gradleLocalProperties(rootDir).getProperty("sonatypeUsername")
     val sonatypePassword = gradleLocalProperties(rootDir).getProperty("sonatypePassword")
     val gpgKeySecret = gradleLocalProperties(rootDir).getProperty("gpgKeySecret")
