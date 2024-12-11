7dc5c7f8e3e0bd546ef10b3b8bd8c22676f5718a
Mirzamehdi <mirzemehdi@gmail.com>
2024-12-12 00:17:23 +0100

    Update api

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,5 @@
-import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
