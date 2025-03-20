e1c68bfdc66ab51191bdeb5a4aa045cd215cba23
Mirzamehdi <mirzemehdi@gmail.com>
2025-03-20 16:58:10 +0100

    Migrating maven publish to new one

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.jetbrains.dokka.gradle.DokkaTask
-
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -12,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlinNativeCocoaPods) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.kotlinx.binary.validator)
-    alias(libs.plugins.nexusPublish)
+    alias(libs.plugins.mavenPublish) apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
@@ -28,8 +25,6 @@ allprojects {
     group = "io.github.mirzemehdi"
     version = project.properties["kmpNotifierVersion"] as String
 
-    val gpgKeySecret = gradleLocalProperties(rootDir, providers).getProperty("gpgKeySecret")
-    val gpgKeyPassword = gradleLocalProperties(rootDir, providers).getProperty("gpgKeyPassword")
 
     val excludedModules = listOf(":sample")
     if (project.path in excludedModules) return@allprojects
@@ -37,70 +32,9 @@ allprojects {
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
-
-
-    extensions.configure<PublishingExtension> {
-        val javadocJar = tasks.register<Jar>("javadocJar") {
-            dependsOn(tasks.getByName<DokkaTask>("dokkaHtml"))
-            archiveClassifier.set("javadoc")
-            from("${layout.buildDirectory}/dokka")
-        }
-
-        publications {
-            withType<MavenPublication> {
-                artifact(javadocJar)
-                pom {
-                    groupId="io.github.mirzemehdi"
-                    name.set("KMPNotifier")
-                    description.set(" Kotlin Multiplatform Push Notification Library targeting ios and android")
-                    licenses {
-                        license {
-                            name.set("Apache-2.0")
-                            url.set("https://opensource.org/licenses/Apache-2.0")
-                        }
-                    }
-                    url.set("mirzemehdi.github.io/KMPNotifier/")
-                    issueManagement {
-                        system.set("Github")
-                        url.set("https://github.com/mirzemehdi/KMPNotifier/issues")
-                    }
-                    scm {
-                        connection.set("https://github.com/mirzemehdi/KMPNotifier.git")
-                        url.set("https://github.com/mirzemehdi/KMPNotifier")
-                    }
-                    developers {
-                        developer {
-                            name.set("Mirzamehdi Karimov")
-                            email.set("mirzemehdi@gmail.com")
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    val publishing = extensions.getByType<PublishingExtension>()
-    extensions.configure<SigningExtension> {
-        useInMemoryPgpKeys(gpgKeySecret, gpgKeyPassword)
-        sign(publishing.publications)
-    }
-
-    // TODO: remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
-    project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
-        dependsOn(project.tasks.withType(Sign::class.java))
-    }
 }
-nexusPublishing {
-    repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            val sonatypeUsername = gradleLocalProperties(rootDir, providers).getProperty("sonatypeUsername")
-            val sonatypePassword = gradleLocalProperties(rootDir, providers).getProperty("sonatypePassword")
-            username = sonatypeUsername
-            password = sonatypePassword
-        }
-    }
-}
+
+
+
 
 
